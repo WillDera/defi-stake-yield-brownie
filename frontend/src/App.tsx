5d37ecc49b49db3aad08ecab7b6f2d9fe9f8bf89
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-02-09 04:33:29 +0100

    summary

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
-import { DAppProvider, ChainId } from "@usedapp/core"
-import { Header } from "./components/Header"
-import { Container } from "@mui/material"
-import { Main } from "./components/Main"
+import React from "react";
+import "./App.css";
+import { DAppProvider, ChainId } from "@usedapp/core";
+import { Header } from "./components/Header";
+import { Container } from "@mui/material";
+import { Main } from "./components/Main";
 
 function App() {
   return (
-    <DAppProvider config={{
-      supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
-    }}>
+    <DAppProvider
+      config={{
+        supportedChains: [ChainId.Kovan, ChainId.Rinkeby],
+        notifications: {
+          expirationPeriod: 1000,
+          checkInterval: 1000,
+        },
+      }}
+    >
       <div className="App">
-        <Header/>
+        <Header />
         <Container>
-          
           Hello
           <Main />
         </Container>
